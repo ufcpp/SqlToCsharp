5c107713314753cd16e8d1578c0b80dd94abe281
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-09-26 16:34:27 +0900

    SDK-based csproj に変更
    ついでに、net472 にした。

---
+++ src/SqlToCsharp/ValueTupleExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SqlToCsharp
+{
+    using System.Collections.Generic;
+
+    static class TupleExtensions
+    {
+        public static IEnumerable<(T item, int index)> Indexed<T>(this IEnumerable<T> items)
+        {
+            int index = 0;
+            foreach (var item in items)
+            {
+                yield return (item, index);
+                ++index;
+            }
+        }
+    }
+}
